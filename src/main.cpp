b25ffc8a43b01c43e73cba259274563da2aced41
lostybtw <aarnav.chaplot@gmail.com>
2023-05-30 09:14:14 +0530

    learning ncurses

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,16 @@ using namespace std;
 
 int main(int argc, char *argv[]){
 	initscr();
+	int x,y;
+	x = y = 10;
+	move(y,x);
 	printw("hello world!");
 	refresh();
 	int key = getch();
+	clear();
+	move(0,0);
 	printw("%d", key);
+	refresh();
 	getch();
 	endwin();
 	return 0;
